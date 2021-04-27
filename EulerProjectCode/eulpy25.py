22643d91260b79a47d76b702b11bb765b1ee89dd
Paul Joseph Devlin <devunderslash@users.noreply.github.com>
2021-04-27 08:57:21 +0100

    Code comments update

--- EulerProjectCode/eulpy25.py
+++ EulerProjectCode/eulpy25.py
@@ -1,17 +1,4 @@
 # # Project Euler Problem 25 - https://projecteuler.net/problem=25
-# 
-# The Fibonacci sequence runs as follows:
-# The numbers increase by adding the latest 2 numbers in the sequence to get the next number.
-# For example the sequence of 1, 1, 2, 3, 5, 8, 13 is produced like so in a simplified view:
-#             1
-#             1
-#     1 + 1 = 2
-#     1 + 2 = 3
-#     2 + 3 = 5
-#     3 + 5 = 8
-#     5 + 8 = 13
-#
-# For this solution the index (term) of the first number to contain 1000 digits will be printed out
 
 def compute():
 # Since we know that the first two terms are going to be  1, 1 we can set the start points as follows
